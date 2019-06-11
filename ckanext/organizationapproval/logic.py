b360e619c1d0b16f48b62a6ea8e79e0973f9a967
Aaron Hakala <aaron.hakala@gmail.com>
2019-06-11 15:25:09 +0200

    AV-783: added deny reason popup, denied email, requesting reapproval button and organization status banner

--- ckanext/organizationapproval/logic.py
+++ ckanext/organizationapproval/logic.py
@@ -9,16 +9,34 @@ from ckan.lib.base import render_jinja2
 log = logging.getLogger(__name__)
 
 
-def send_organization_approved(organization):
+def get_user_from_organization(organization):
     from ckan import model
-
     context = {'model': model, 'ignore_auth': True, 'session': model.Session}
-    site_addr = config['ckan.site_url']
 
     user_id = organization['users'][0]['id']
-    user_details = get_action('user_show')(context, data_dict={"id": user_id})
+    return get_action('user_show')(context, data_dict={"id": user_id})
 
+
+def send_organization_approved(organization):
+    user_details = get_user_from_organization(organization)
+    site_addr = config['ckan.site_url']
     email = make_email_template('organization_approved', {
+      "url_guide": site_addr + '/opas/avoimen-datan-opas',
+      "url_user_guide": site_addr + '/opas/johdanto'
+    })
+
+    send_email(
+        user_details['name'],
+        user_details['email'],
+        email['subject'],
+        email['message'])
+
+
+def send_organization_denied(organization, reason):
+    user_details = get_user_from_organization(organization)
+    site_addr = config['ckan.site_url']
+    email = make_email_template('organization_denied', {
+      "reason": reason,
       "url_guide": site_addr + '/opas/avoimen-datan-opas',
       "url_user_guide": site_addr + '/opas/johdanto'
     })
